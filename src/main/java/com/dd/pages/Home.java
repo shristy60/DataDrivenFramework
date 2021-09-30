b17900f8e232d8f8b83b96565bb26fe6bde409e0
shristy chouhan <shristy.chouhan4@gmail.com>
2021-09-30 16:14:48 -0400

    added contacts page class, its objects and actions

--- src/main/java/com/dd/pages/Home.java
+++ src/main/java/com/dd/pages/Home.java
@@ -1,6 +1,57 @@
 package com.dd.pages;
 
 import com.dd.base.TestBase;
+import com.dd.util.TestUtil;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.security.PublicKey;
 
 public class Home extends TestBase {
+    TestUtil testUtil;
+
+    @FindBy(xpath = "//div/span[contains(text(),'Shristy Chouhan')]")
+    WebElement userNameLabel;
+
+    @FindBy(xpath="//div/a[@href='/contacts']")
+    WebElement contactsLink;
+
+    @FindBy(xpath="//a[@href='/deals']")
+    WebElement dealsLink;
+
+    @FindBy(xpath="//a[@href='/tasks']")
+    WebElement tasksLink;
+
+    //Initializing the Home page objects
+    public Home(){
+        PageFactory.initElements(driver,this);
+        testUtil = new TestUtil();
+    }
+
+    public String verifyHomePageTitle(){
+        return driver.getTitle();
+    }
+    public boolean verifyUserName(){
+        return userNameLabel.isDisplayed();
+    }
+
+    public ContactsPage clickOnContacts(){
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript("scroll(0,300)");
+        testUtil.actions(contactsLink);
+        contactsLink.click();
+        return new ContactsPage();
+    }
+
+    public DealsPage clickOnDeals(){
+        dealsLink.click();
+        return new DealsPage();
+    }
+    public TasksPage clickOnTasks(){
+        tasksLink.click();
+        return new TasksPage();
+    }
+
 }
--- src/main/java/com/dd/util/TestUtil.java
+++ src/main/java/com/dd/util/TestUtil.java
@@ -1,6 +1,17 @@
 package com.dd.util;
 
-public class TestUtil {
+import com.dd.base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class TestUtil extends TestBase {
      public static long PAGE_LOAD = 20;
      public static long IMPLICIT_WAIT = 10;
+
+     public void actions(WebElement element){
+          Actions action = new Actions(driver);
+          //Action mouseOver = action.moveToElement(element).click().perform();
+          action.moveToElement(element).click().perform();
+     }
 }
